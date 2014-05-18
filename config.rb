8342638d70e5197352d62c7c313aad745073c488
Jose Gonzalez <jose@digitalnerd.net>
2014-05-17 18:24:01 -0700

    first commit

--- config.rb
+++ config.rb
@@ -1,9 +1,12 @@
 # Require any additional compass plugins here.
 add_import_path "bower_components/foundation/scss"
+add_import_path "bower_components/foundation/js"
+add_import_path "bower_components/modernizr"
+add_import_path "bower_components/jquery/dist"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
